c76c13d7e1b757d59a919ef27b037aca49c8ddf4
charge72002 <bwong24@wisc.edu>
2022-02-11 00:50:34 -0600

    Improved video making/plotting

--- athenavisualizer.py
+++ athenavisualizer.py
@@ -26,12 +26,15 @@ import athena_read as ath
 
 
 #%%
-filename = "/Users/bwong/URS_Athena_Cpp/SWONG_shock_tube_bump_HDF5/Sod.out1.00000.athdf"
+filename = "/Users/bwong/URS_Athena_Cpp/SWONG_shock_tube_bump_HDF5/Sod.out1.00010.athdf"
 data = ath.athdf(filename)
 print(data.keys())
 x = data['x1v']
 y = data['rho'][0][0]
 plt.plot(x, y)
+plt.title("Density (computational units)")
+plt.xlabel("density")
+plt.ylabel("x position")
 
 #%%
 
@@ -47,7 +50,7 @@ for i in range(0, 125):
 #%%
 #https://stackoverflow.com/questions/4092927/generating-movie-from-python-without-saving-individual-frames-to-files
 xKey = 'x1v'
-yKey = 'press'
+yKey = 'combo'
 
 filename = "/Users/bwong/URS_Athena_Cpp/SWONG_shock_tube_bump_HDF5/Sod.out1.00000.athdf"
 data = ath.athdf(filename)
@@ -69,17 +72,27 @@ def update_img(n):
     ax.set_ylim([0, 2.5])
     inFile = (("%s/%s.out%1i.%05i.%s") % (pwd+"/SWONG_shock_tube_bump_HDF5","Sod",1,n,"athdf"))
     data = ath.athdf(inFile)
-    x = data[xKey]
-    y = data[yKey][0][0]
-    im = ax.plot(x, y)
+    if yKey == "combo":
+        x = data[xKey]
+        for yKey2 in ['rho', 'press', 'vel1']:
+            y = data[yKey2][0][0]
+            im = ax.plot(x, y, label=yKey2)
+        ax.legend()
+    else:
+        x = data[xKey]
+        y = data[yKey][0][0]
+        im = ax.plot(x, y)
+        ax.set_ylabel(yKey2)
+    ax.set_title(yKey + "(computational units)")
+    ax.set_xlabel("x position (cm)")
     return im
 
 #legend(loc=0)
 TEMPani = ani.FuncAnimation(fig,update_img,125,interval=1)
 writer = ani.writers['ffmpeg'](fps=30)
 
-TEMPani.save(pwd + '/'+yKey+'.mp4',writer=writer,dpi=100)
-print("Saved as " + pwd + 'demo.mp4')
+TEMPani.save(pwd + '/' + yKey+'.mp4',writer=writer,dpi=100)
+print("Saved as " + pwd + '/' + yKey+'.mp4')
 #return ani
 
 #ani_frame()
